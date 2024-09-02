271bed634624fdc447f6f43339cb20afbf2aa879
dinellipaulo <pgsdinelli@gmail.com>
2024-09-02 04:08:51 -0300

    Classe TodoService implementada

--- src/main/java/br/com/paulodinelli/todolist/service/TodoService.java
+++ src/main/java/br/com/paulodinelli/todolist/service/TodoService.java
@@ -1,7 +1,38 @@
 package br.com.paulodinelli.todolist.service;
 
+import br.com.paulodinelli.todolist.entities.Todo;
+import br.com.paulodinelli.todolist.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TodoService {
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> create(Todo todo){
+        todoRepository.save(todo);
+        return read();
+    }
+
+    public List<Todo> read(){
+        Sort sort = Sort.by("priority").descending().and(
+                Sort.by("name").ascending());
+        return todoRepository.findAll();
+    }
+
+    public List<Todo> update(Todo todo){
+        todoRepository.save(todo);
+        return read();
+    }
+
+    public List<Todo> delete(Long id){
+        todoRepository.deleteById(id);
+        return read();
+    }
+
+
 }
